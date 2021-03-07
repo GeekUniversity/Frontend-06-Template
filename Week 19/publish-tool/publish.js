859a6559ef664d8861f16eae8209bc3e5824cc55
IronPanX <rxcqcn@foxmail.com>
2021-03-07 19:41:51 +0800

    week 19

--- Week 19/publish-tool/publish.js
+++ Week 19/publish-tool/publish.js
@@ -2,8 +2,38 @@ let http = require('http');
 let fs = require('fs');
 let archiver = require('archiver');
 let child_process = require('child_process');
+let querystring = require('querystring');
 
 child_process.exec(`open https://github.com/login/oauth/authorize?client_id=`)
+
+http.createServer(function (request, response) {
+    let query = querystring.parse(request.url.match(/^\/\?([\s\S]+)$/)[1]);
+    console.log(query)
+}).listen(8083);
+
+function publish(token) {
+    let request = http.request({
+        hostname: "127.0.0.1",
+        port: 8082,
+        method: "POST",
+        path:"/publish?token=" + token,
+        headers: {
+            'Content-Type': 'application/octet-Stream',
+        }
+    }, response => {
+        console.log(response);
+    });
+
+    const archive = archiver('zip', {
+        zlib: {level: 9}
+    })
+
+    archive.directory('./sample/', false);
+
+    archive.finalize();
+
+    archive.pipe(request)
+}
 
 /*fs.stat("./sample.html",(err, stats) => {
     let request = http.request({
@@ -25,29 +55,11 @@ child_process.exec(`open https://github.com/login/oauth/authorize?client_id=`)
     file.on('end', () => request.end())
 })*/
 
-let request = http.request({
-    hostname: "127.0.0.1",
-    port: 8082,
-    method: "POST",
-    headers: {
-        'Content-Type': 'application/octet-Stream',
-        // 'Content-Length': stats.size
-    }
-}, response => {
-    console.log(response);
-});
 
-let file = fs.createReadStream("./sample.html")
 
-const archive = archiver('zip', {
-    zlib: {level: 9}
-})
+// let file = fs.createReadStream("./sample.html")
 
-archive.directory('./sample/', false);
 
-archive.finalize();
-
-archive.pipe(request)
 
 
 /*file.pipe(request)
